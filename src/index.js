008d7cc0ee97a6f8883acbb76dd9c5f79f73d0ef
Md Areeb Ansar <mdareebansar@gmail.com>
2023-12-17 14:45:09 +0530

    Reverting route changes as they have been handled on api gateway side using pathRewrite

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ app.use(express.json()); //Helps to parse the incoming request body as json
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
-app.use("/flightsservice/api", apiRoutes);
-app.get("/flightsservice/home", (req, res) => {
-  console.log(req);
-  return res.json({ msg: "ok" });
-});
+// app.use("/flightsservice/api", apiRoutes);
+// app.get("/flightsservice/home", (req, res) => {
+//   console.log(req);
+//   return res.json({ msg: "ok" });
+// });
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Server started at PORT: ${ServerConfig.PORT}`);
   Logger.info("Server up and running.");
